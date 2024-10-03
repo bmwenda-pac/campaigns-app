39e998120cf3263ba4f12f07c4872bc502743e52
bmwenda-pac <bmwenda@paciskenya.com>
2024-10-03 15:02:08 +0300

    Set up cron-job

---
+++ app/api/services/scheduler/route.ts
@@ -0,0 +1,20 @@
+import { lucia } from "@/lib/lucia";
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function GET(request: Request) {
+  try {
+    console.log("");
+    console.log("######################################");
+    console.log("#                                    #");
+    console.log("# Running scheduler every 20 minutes #");
+    console.log("#                                    #");
+    console.log("######################################");
+    console.log("");
+    await lucia.deleteExpiredSessions();
+    return NextResponse.json({ message: "Success" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
